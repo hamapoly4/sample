c7dcaea3ca7927e202f77d6414e88086e4a955f7
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-05-31 13:26:54 +0900

    Add files via upload

---
+++ sample03/app/Tracer.cpp
@@ -0,0 +1,34 @@
+#include "Tracer.h" // <1>
+
+Tracer::Tracer():
+  leftWheel(PORT_C), rightWheel(PORT_B), colorSensor(PORT_3) { // <2>
+  }
+
+void Tracer::init() {
+  init_f("Tracer");
+}
+
+void Tracer::terminate() {
+  msg_f("Stopped.", 1);
+  leftWheel.stop();  // <1>
+  rightWheel.stop();
+}
+
+
+  float Tracer::calc_prop_value(){
+  const float Kp = 0.8;
+  const int target = 35;
+  const int bias = 5;
+
+    int diff = colorSensor.getBrightness() - target;
+    return (Kp * diff + bias);
+  }
+
+void Tracer::run() {
+  msg_f("runnning...", 1);
+  float turn = calc_prop_value();
+  int pwm_l = pwm - turn;
+  int pwm_r = pwm + turn;
+  leftWheel.setPWM(pwm_l);
+  rightWheel.setPWM(pwm_r);
+}
